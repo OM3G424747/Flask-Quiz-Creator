e63f0a966fcaabdfd458b2317f1601447e9b4297
Chris Joubert <44314634+OM3G424747@users.noreply.github.com>
2022-01-11 19:58:32 +0200

    updated schema with new data structure

--- schema.py
+++ schema.py
@@ -5,18 +5,115 @@ import sqlite3
 connection = sqlite3.connect("flask_tut.db", check_same_thread = False)
 cursor = connection.cursor()
 
+# Create User Table
 cursor.execute(
     """
-    CREATE TABLE users(
-    pk INTEGER PRIMARY KEY AUTOINCREMENT,
-    username VARCHAR(32),
-    password VARCHAR(32),
-    email VARCHAR(32),
-    secret_word VARCHAR(32)
+    CREATE TABLE users (
+        id_num INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(255),
+        email VARCHAR(255),
+        pass VARCHAR(255),
+        firstname VARCHAR(255),
+        lastname VARCHAR(255),
+        displayname BOOLEAN
     );
     """
 )
-
 connection.commit()
 cursor.close()
 connection.close()
+
+
+connection = sqlite3.connect("flask_tut.db", check_same_thread = False)
+cursor = connection.cursor()
+
+# Create Quiz Table
+cursor.execute(
+    """
+    CREATE TABLE quiz (
+        quiz_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id_num INTEGER,
+        quiz_name VARCHAR(255),
+        total_questions INTEGER
+    );
+    """
+)
+connection.commit()
+cursor.close()
+connection.close()
+
+
+connection = sqlite3.connect("flask_tut.db", check_same_thread = False)
+cursor = connection.cursor()
+
+# Create Question Table
+cursor.execute(
+    """
+    CREATE TABLE question (
+        question_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        quiz_id INTEGER,
+        questions_text VARCHAR(255),
+        total_options INTEGER
+    );
+    """
+)
+connection.commit()
+cursor.close()
+connection.close()
+
+
+connection = sqlite3.connect("flask_tut.db", check_same_thread = False)
+cursor = connection.cursor()
+
+# Create Answers Table
+cursor.execute(
+    """
+    CREATE TABLE answers (
+        question_id INTEGER,
+        answer_text VARCHAR(255)
+    );
+    """
+)
+connection.commit()
+cursor.close()
+connection.close()
+
+
+
+connection = sqlite3.connect("flask_tut.db", check_same_thread = False)
+cursor = connection.cursor()
+
+# Create Access Table
+cursor.execute(
+    """
+    CREATE TABLE access (
+        creator_id INTEGER,
+        quiz_id INTEGER,
+        testee_id INTEGER
+    );
+    """
+)
+connection.commit()
+cursor.close()
+connection.close()
+
+
+connection = sqlite3.connect("flask_tut.db", check_same_thread = False)
+cursor = connection.cursor()
+
+# Create Score Table
+cursor.execute(
+    """
+    CREATE TABLE score (
+        quiz_id INTEGER,
+        testee_id INTEGER,
+        score INTEGER
+    );
+    """
+)
+connection.commit()
+cursor.close()
+connection.close()
+
+
+
